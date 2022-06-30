22b8781d2313f65ef1e20a230c32f0158fbe55d1
uiuc-earable-computing <59186998+uiuc-earable-computing@users.noreply.github.com>
2022-06-30 14:16:48 +0800

    Add files via upload
    update website

---
+++ js/init.js
@@ -0,0 +1,44 @@
+// Keep track of all loaded buffers.
+var BUFFERS = {};
+// Page-wide audio context.
+var context = null;
+
+// An object to track the buffers to load {name: path}
+var BUFFERS_TO_LOAD = {
+    spkr1: 'src/crossfade/1378_sph1_mimo_sagrnn.wav',
+    spkr2: 'src/crossfade/1378_sph2_mimo_sagrnn.wav',
+    spkr1_mono: 'src/crossfade/1378_sph1_mimo_sagrnn_mono.wav',
+    spkr2_mono: 'src/crossfade/1378_sph2_mimo_sagrnn_mono.wav'
+};
+
+// Loads all sound samples into the buffers object.
+function loadBuffers() {
+  // Array-ify
+  var names = [];
+  var paths = [];
+  for (var name in BUFFERS_TO_LOAD) {
+    var path = BUFFERS_TO_LOAD[name];
+    names.push(name);
+    paths.push(path);
+  }
+  bufferLoader = new BufferLoader(context, paths, function(bufferList) {
+    for (var i = 0; i < bufferList.length; i++) {
+      var buffer = bufferList[i];
+      var name = names[i];
+      BUFFERS[name] = buffer;
+    }
+  });
+  bufferLoader.load();
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  try {
+    // Fix up prefixing
+    window.AudioContext = window.AudioContext || window.webkitAudioContext;
+    context = new AudioContext();
+  }
+  catch(e) {
+    alert("Web Audio API is not supported in this browser");
+  }
+  loadBuffers();
+});
